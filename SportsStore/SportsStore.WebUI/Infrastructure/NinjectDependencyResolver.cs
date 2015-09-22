75a3e2e9ab4eb6e1c1cfa144a9cca435a7b65d62
Eddie Rangel <eddie.rangel@doverals.com>
2015-09-22 14:36:14 -0700

    Configuring Ninject

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            //Adding Bindings
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+    }
+}
