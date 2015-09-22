fa3fcba03e3387c0474696efe5ed25d2ff93fffe
Eddie Rangel <eddie.rangel@doverals.com>
2015-09-22 15:40:14 -0700

    Created Product Controller and View. Including all View Items.

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _productRepository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_productRepository.Products);
+        }
+
+    }
+}
